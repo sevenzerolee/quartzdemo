5186bddb51f3c0ef47adc2e1f4b6b1eed0598ba4
libin <libin2@ctsi.com.cn>
2018-01-31 10:43:15 +0800

    trigger使用 防止并发 输出任务执行次数 Misfire(错失触发）策略(默认的方式)

--- src/main/java/org/sevenzero/quartz/quartzdemo/HelloExample.java
+++ src/main/java/org/sevenzero/quartz/quartzdemo/HelloExample.java
@@ -3,6 +3,7 @@ package org.sevenzero.quartz.quartzdemo;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
+import org.quartz.CronScheduleBuilder;
 import org.quartz.DateBuilder;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
@@ -19,50 +20,60 @@ public class HelloExample {
 
 	public static void main(String[] args) throws SchedulerException {
 
-		log.info("------- Initializing ----------------------");
+		log.warn("------- Initializing ----------------------");
 
 	    // First we must get a reference to a scheduler
 	    SchedulerFactory sf = new StdSchedulerFactory();
 	    Scheduler sched = sf.getScheduler();
+	    sched.clear();
 
-	    log.info("------- Initialization Complete -----------");
+	    log.warn("------- Initialization Complete -----------");
 
 	    // computer a time that is on the next round minute
 	    Date runTime = DateBuilder.evenMinuteDate(new Date());
 
-	    log.info("------- Scheduling Job  -------------------");
+	    log.warn("------- Scheduling Job  -------------------");
 
 	    // define the job and tie it to our HelloJob class
 	    JobDetail job = JobBuilder.newJob(HelloJob.class).withIdentity("job1", "group1").build();
+//	    JobDetail job = JobBuilder.newJob(HelloJob.class).withIdentity("job1", "group1").
+//	    		usingJobData(HelloJob.KEY_NUM_EXECUTIONS, "0").build();
+	    
+//	    job.getJobDataMap().put(HelloJob.KEY_NUM_EXECUTIONS, "0");
 
 	    // Trigger the job to run on the next round minute
-	    Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startAt(runTime).build();
+//	    Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startAt(runTime).build();
+	    CronScheduleBuilder csb = CronScheduleBuilder.cronSchedule("0/3 * * * * ?");
+	    Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
+	    		.withSchedule(csb).build();
 
 	    // Tell quartz to schedule the job using our trigger
 	    sched.scheduleJob(job, trigger);
-	    log.info(job.getKey() + " will run at: " + runTime);
+	    log.warn(job.getKey() + " will run at: " + runTime);
 
 	    // Start up the scheduler (nothing can actually run until the
 	    // scheduler has been started)
 	    sched.start();
 
-	    log.info("------- Started Scheduler -----------------");
+	    log.warn("------- Started Scheduler -----------------");
 
 	    // wait long enough so that the scheduler as an opportunity to
 	    // run the job!
-	    log.info("------- Waiting 65 seconds... -------------");
-	    try {
-	      // wait 65 seconds to show job
-	      Thread.sleep(65L * 1000L);
-	      // executing...
-	    } catch (Exception e) {
-	      //
-	    }
+		log.warn("------- Waiting 65 seconds... -------------");
+		try {
+			// wait 65 seconds to show job
+			Thread.sleep(7200L * 1000L);
+			// executing...
+		} 
+		catch (Exception e) {
+			//
+			e.printStackTrace();
+		}
 
 	    // shut down the scheduler
-	    log.info("------- Shutting Down ---------------------");
+	    log.warn("------- Shutting Down ---------------------");
 	    sched.shutdown(true);
-	    log.info("------- Shutdown Complete -----------------");
+	    log.warn("------- Shutdown Complete -----------------");
 		
 	}
 
